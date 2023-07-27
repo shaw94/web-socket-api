abc8d3628601b808e4faedf1ad262de61c9a541e
Mohamed Shaw <mohamedShawD08@gmail.com>
2023-07-27 10:02:25 +0200

    Added Broadcast routes in API route including sanctum

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Validation\ValidationException;
 
 /*
@@ -16,6 +17,8 @@ use Illuminate\Validation\ValidationException;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Broadcast::routes(['middleware' => ['auth:sanctum']]);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
